a7a6bda43675edf403f1c261c1d4f8c65318abe8
unknown <master_here100@yahoo.com>
2018-09-27 14:57:56 +0200

    remove folder

--- admin/includes/templates/footer.php
+++
@@ -1,9 +0,0 @@
-		<div class="footer">
-			
-		</div>
-
-		<script type="text/javascript" src="<?php echo $js; ?>jquery-1.12.1.min.js"></script>
-		<script type="text/javascript" src="<?php echo $js; ?>bootstrap.min.js"></script>
-		<script type="text/javascript" src="<?php echo $js; ?>backend.js"></script>
-	</body>
-</html>
